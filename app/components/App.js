f3bc93c5500cb9d5078dfb3708f7811d04eeab2a
ChadoNihi <chadoishere@gmail.com>
2016-11-21 14:01:01 +0200

    save

--- app/components/App.js
+++ app/components/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
   }
 
   componentDidMount () {
-    this.props.dispatch(fetchPolls());
-    this.props.dispatch(fetchUser());
+    /*this.props.dispatch(fetchPolls());
+    this.props.dispatch(fetchUser());*/
   }
 
   render() {
@@ -20,15 +20,15 @@ class App extends React.Component {
     try {
       ch = React.Children.only(this.props.children);
     } catch (e) {
-      ch = undefined;
+      ch = null;
     }
-    if (!this.props.user) {
+    /*if (!this.props.user) {
       this.props.dispatch(fetchUser()); // TODO: find another way
-    }
+    }*/
     console.log("this.props.user: "+this.props.user);
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--no-drawer-button">
-        <Header isLoggedIn={!!this.props.user} title="The Voting App" subtitle={(ch && ch.type == "UserMain" ? "Your polls" : undefined)} />
+        <Header isLoggedIn={!!this.props.user} title="The Voting App" subtitle={(ch && ch.type == "UserMain" ? "Your polls" : null)} />
         <main className="mdl-layout__content">
           {this.props.children}
           <Footer />
